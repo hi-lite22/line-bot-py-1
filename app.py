c2af2f695ba211d2083112e491e3ec9c6d91544c
hi-lite22 <mac.webapp22@gmail.com>
2020-04-17 17:29:19 +0900

    updete

--- app.py
+++ app.py
@@ -12,31 +12,26 @@ from linebot.models import (
 import os
 
 app = Flask(__name__)
+app.debug = False
 
-#環境変数取得
-YOUR_CHANNEL_ACCESS_TOKEN = os.environ["kDji1+bU35yIn5K2O4hx6UhpjlgxAtBdOVgeSCyK62+g9aYXSV2FzSbziBpnrr5vbQBNhNS4/a2juuzs0/Qit4aN7Djwk6TNpWhL4HBBOHIXXDAXqOUpXZaZxTp6TAD73LWON6auA4gvBlMYRkJw+gdB04t89/1O/w1cDnyilFU="]
-YOUR_CHANNEL_SECRET = os.environ["7c0aa75db34d80d9149c60a2576c6f61"]
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-
     return 'OK'
-
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -44,8 +39,6 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=event.message.text))
 
-
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
