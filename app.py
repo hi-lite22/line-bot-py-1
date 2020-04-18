724115f9aaabe7d19b8dcbf204892fcf9b7274f0
hi-lite22 <mac.webapp22@gmail.com>
2020-04-18 21:09:23 +0900

    debug

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
+@app.route("/index", methods=['POST'])
+def index():
+    return 'OK'
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
